be96a1ef8da1a1d1b7eca71cde792b7257b3d881
Jamaal-Fisher <bathwatermelody@gmail.com>
2022-05-27 08:22:00 -0400

    updating repo

--- p1-assignment/CevicheAPI/Controllers/RestaurantController.cs
+++ p1-assignment/CevicheAPI/Controllers/RestaurantController.cs
@@ -1,43 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CevicheAPI.Controllers
 {
+    //  Routes are used to configure the endpoints of the API
     [Route("api/[controller]")]
     [ApiController]
     public class RestaurantController : ControllerBase
     {
-        // GET: api/<ValuesController>
+        // Action Methods ways to access or manipulate the resources, HTTP verbs/methods (GET, PUT, POST, DELETE, PATCH, HEAD)
+        // Attribute explicitly defines the function.  can be seen in Postman.
         [HttpGet]
-        public IEnumerable<string> Get()
+        public string Get()
         {
-            return new string[] { "value1", "value2" };
+            return "Hello Restaurant!";
         }
 
-        // GET api/<ValuesController>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/<ValuesController>
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT api/<ValuesController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/<ValuesController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
     }
 }
